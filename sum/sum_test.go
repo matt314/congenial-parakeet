a18b1af023dff9878bbcaaa95cf4881543869dae
Matt Piechota <matthew.piechota@lmco.com>
2022-01-09 14:55:24 -0500

    Implemented with array

---
+++ sum/sum_test.go
@@ -0,0 +1,13 @@
+package sum
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("want %d, got %d, given %v", want, got, numbers)
+	}
+}
